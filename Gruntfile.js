42eea4641255d5c596d9894d0491bfb0a1c5d2ac
Joao Stein <joaosteing@gmail.com>
2014-01-04 15:49:01 -0800

    Add grunt-contrib-uglify responsible for minify files

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,14 +1,26 @@
 module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+
+    // Concatenate files
     concat: {
       dist: {
         src: ['public/javascripts/*.js'],
         dest: 'public/production/application.js'
       }
+    },
+
+    // Minify files
+    uglify: {
+      build: {
+        src: 'public/production/application.js',
+        dest: 'public/production/application.min.js'
+      }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.registerTask('default', ['concat']);
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  grunt.registerTask('default', ['concat', 'uglify']);
 };
