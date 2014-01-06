d9c1ff4249f620eebf6f1e30051dab3c23f59c1f
Joao Stein <joaosteing@gmail.com>
2014-01-05 16:39:58 -0800

    Add basic crud with mongo

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var routes = require('./routes');
 var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
+var mongoose = require('mongoose');
 
 var app = express();
 
@@ -21,6 +22,83 @@ app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Mongoose
+mongoose.connect('mongodb://localhost/themeritshop');
+
+var UserSchema = mongoose.Schema({
+  name: String,
+  email: String,
+  address: String
+});
+
+var User = mongoose.model('User', UserSchema);
+
+// Show
+app.get('/users', function (req, res) {
+  User.find({}, function (err, docs) {
+    res.render('users/index', { users: docs });
+  });
+});
+
+// New
+app.get('/users/new', function (req, res) {
+  res.render('users/new');
+});
+
+// Create
+app.post('/users', function (req, res) {
+  var body = req.body;
+
+  var user = new User ({
+    name: body.name,
+    email: body.email,
+    address: body.address
+  });
+
+  user.save(function (err, user) {
+    if (err) {
+      res.json(err);
+    }
+    res.redirect('/users/' + user.name);
+  });
+});
+
+app.param('name', function (req, res, next, name) {
+  User.find({ name: name }, function (err, docs) {
+    req.user = docs[0];
+    next();
+  });
+});
+
+// Show
+app.get('/users/:name', function (req, res) {
+  res.render('users/show', { user: req.user });
+});
+
+// Edit
+app.get('/users/:name/edit', function (req, res) {
+  res.render('users/edit', { user: req.user });
+});
+
+// Update
+app.put('/users/:name', function (req, res) {
+  var body = req.body;
+  User.update({ name: req.params.name }, {
+    name: body.name,
+    email: body.email,
+    address: body.address
+  }, function(err) {
+    res.redirect('users/' + body.name);
+  });
+});
+
+// Delete
+app.delete('/users/:name', function (req, res) {
+  User.remove({ name: req.params.name }, function (err) {
+    res.redirect('/users');
+  })
+});
 
 // development only
 if ('development' == app.get('env')) {
